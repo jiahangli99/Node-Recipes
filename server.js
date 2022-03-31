be823583b5c7839cdff6ad7a63497f8d0b502fc1
jiahangli99 <liabraham.1999@gmail.com>
2022-03-31 11:30:12 -0600

    setup mongodb configs

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// DEPENDENCIES
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
@@ -8,7 +9,14 @@ require('dotenv').config()
 const recipesRouter = require('./controllers/recipesRouter')
 
 const PORT = process.env.PORT
+const DATABASE = process.env.DATABASE_URL
 
+// MongoDB Configs
+mongoose.connect(DATABASE)
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
 
 
 app.listen(PORT, () => {
