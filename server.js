c5d2bf8ce7c2d5727b3fa38926a98b52e33d736b
jiahangli99 <liabraham.1999@gmail.com>
2022-03-31 11:25:59 -0600

    setup more dependencies and added controller

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
+const app = express()
+
+require('dotenv').config()
+
+const recipesRouter = require('./controllers/recipesRouter')
+
+const PORT = process.env.PORT
+
+
+
+app.listen(PORT, () => {
+    console.log('listening on ', PORT)
+})
+
+
+
+
+
+
+
